da58d447dccf9257a31d89d893092e25cfbaa802
石垣千尋 <bs_29@ishigakichihironoMacBook.local>
2019-05-01 20:09:42 +0900

    change console for

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 'use strict';
 
-const signal = 'pink';
-
-switch (signal) {
-   case 'red':
-     console.log('stop!');
-     break;
-   case 'yellow':
-     console.log('caution!');
-     break;
-   case 'blue':
-   case 'green':
-     console.log('go!');
-     break;
-   default:
-     console.log('wrong signal!');
-     break;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i}hello`);
 }
