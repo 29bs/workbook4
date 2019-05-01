401cfe1d4fbff3975ae2d3e5871bec4bae813571
石垣千尋 <bs_29@ishigakichihironoMacBook.local>
2019-05-01 19:55:03 +0900

    change console if

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const score = 85;
+const score = 60;
+const name = 'taguchi';
 
-score >= 80 ? console.log('Great!') : console.log('OK')
+if (score >= 50 && name === 'taguchi') {
+  console.log('Good Job!')
+}
