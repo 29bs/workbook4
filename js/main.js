f830a7e9f446382b7f8b8e92d3e72cd3efd10e02
石垣千尋 <bs_29@ishigakichihironoMacBook.local>
2019-05-01 21:08:51 +0900

    change console function const

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 'use strict';
 
-const sum = function(a, b, c) {
-  return a + b + c
-};
-
-sum(1, 2, 3);
-sum(3, 4, 5);
-
-const total = sum(1, 2, 3) + sum(3, 4, 5);
-
-console.log(total);
+const double = (a) => a * 2;
+console.log(double(12));
