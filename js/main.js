d2e3c14225b7b707b71ea93b88c37baa747765a7
石垣千尋 <bs_29@ishigakichihironoMacBook.local>
2019-05-01 20:20:53 +0900

    change console continue break

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 'use strict';
 
-let hp = 200;
-
-while (hp > 0) {
-  console.log(`${hp}HP left!`);
-  hp -= 15;
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0) {
+    continue;
+  }
+  console.log(i);
 }
 
-for (let hp = 200; hp > 0; hp -= 15) {
-  console.log(`${hp}HP left!`);
+for (let i = 1; i <= 10; i++) {
+  if (i === 4) {
+    break;
+  }
+  console.log(i);
 }
